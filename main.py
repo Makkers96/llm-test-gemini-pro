8f63656023a1b2eb6b51160b8805d0e34a6a2ba8
Makkers96 <Makkers96@gmail.com>
2023-12-18 10:51:43 -0500

    function now returns text instead of obj, line spacing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import google.generativeai as genai
 import os
 
-
 key = os.getenv('google_key')
 genai.configure(api_key=key)
 
@@ -13,31 +12,32 @@ generation_config = {
 }
 
 safety_settings = [
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_NONE"
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_NONE"
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_NONE"
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_NONE"
-  }
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE"
+    }
 ]
 
 text_model = genai.GenerativeModel('gemini-pro',
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
+                                   generation_config=generation_config,
+                                   safety_settings=safety_settings)
 
 
 def run_llm(question):
     prompt = f"""{question}"""
-    response = text_model.generate_content(prompt)
+    result = text_model.generate_content(prompt)
+    response = result.text
     print(f"LLM Response: {response}")
     return response
