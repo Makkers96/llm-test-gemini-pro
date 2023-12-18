74c8285952e66c158e3a9d95b964210400863214
Makkers96 <Makkers96@gmail.com>
2023-12-18 09:53:15 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, session
+from main import run_llm
+from IPython.display import display
+from IPython.display import Markdown
+
+app = Flask(__name__)
+app.secret_key = "thisisasupersecretkey124"
+
+
+@app.route("/", methods=["GET", "POST"])
+def google_test():
+
+    if 'llm_response' not in session:
+        session['llm_response'] = ""
+
+    if request.method == 'POST':
+        if 'prompt' in request.form:
+            session['prompt'] = request.form.get('prompt')
+
+            session['llm_response'] = run_llm(session['prompt'])
+
+    return render_template("google_test.html",
+                           llm_response=session['llm_response'],
+                           )
+
+
+@app.route("/markdown", methods=["GET", "POST"])
+def google_test():
+
+    if 'llm_response' not in session:
+        session['llm_response'] = ""
+
+    if request.method == 'POST':
+        if 'prompt' in request.form:
+            session['prompt'] = request.form.get('prompt')
+
+            session['llm_response'] = Markdown(run_llm(session['prompt']).text)
+
+    return render_template("google_test.html",
+                           llm_response=session['llm_response'],
+                           )
+
+if __name__ == "__main__":
+    app.run()
---
+++ main.py
@@ -0,0 +1,43 @@
+import google.generativeai as genai
+from google.colab import userdata
+import os
+
+
+key = os.getenv('google_key')
+genai.configure(api_key=key)
+
+generation_config = {
+    "temperature": 0,
+    "max_output_tokens": 2048,
+    "top_p": 1,
+    "top_k": 1,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_NONE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_NONE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_NONE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_NONE"
+  }
+]
+
+text_model = genai.GenerativeModel('gemini-pro',
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+
+def run_llm(prompt):
+    response = text_model.generate_content(prompt)
+    print(f"LLM Response: {response}")
+    return response
